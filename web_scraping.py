13879726ca9d5245627ef155b43cf5b6d8323a0e
tomi-Ryu <ryusuke.tomita.forjob@gmail.com>
2025-02-27 16:02:10 +0900

    e-naviログイン

--- web_scraping.py
+++ web_scraping.py
@@ -1,11 +1,42 @@
+import json
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-options = Options()
+enavi_Auth = json.load(open("./secret/enavi_secret.json", "r", encoding="utf-8"))
+user_Id = enavi_Auth["e-navi_login"]["user_id"]
+password = enavi_Auth["e-navi_login"]["password"]
+
+def login(driver):
+  # ログインページ / ユーザid入力
+  driver.get('https://www.rakuten-card.co.jp/e-navi/auth/login.xhtml')
+  time.sleep(1)
+  id_InputElement = driver.find_element(By.ID, "user_id")
+  id_InputElement.clear()
+  id_InputElement.send_keys(user_Id)
+
+  toInputPwd_btn = driver.find_element(By.ID, "cta001")
+  toInputPwd_btn.click()
+  time.sleep(1)
+
+  # ログインページ / パスワード入力
+  pwd_InputElement = driver.find_element(By.NAME, "password")
+  pwd_InputElement.clear()
+  pwd_InputElement.send_keys(password)
+
+  login_btn = driver.find_element(By.ID, "cta011")
+  login_btn.click()
+  time.sleep(5)
+
+  # スクショでログイン成功か確認
+  driver.get_screenshot_as_file("./screenshots/login_or_not.png")
+
+
+# FireFoxブラウザをGUIなしで利用
+options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
-# Chromeを起動する
-driver = webdriver.Chrome(options=options)
-# Googleのページを開く
-driver.get('https://www.google.com')
-driver.get_screenshot_as_file("hoge.png")
+driver = webdriver.Firefox(options=options)
+
+login(driver)
+
 driver.quit()
