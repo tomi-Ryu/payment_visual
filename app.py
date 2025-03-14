edb63c444d0dadc86fad00342920dd0671a90c35
tomi-Ryu <ryusuke.tomita.forjob@gmail.com>
2025-03-14 19:44:03 +0900

    bottleサーバー立ち上げ時の前後のみ、db接続と接続切断をする

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-import bottle
-import routes
+# DB接続
+import json
+import pymysql
+from bottle import Bottle
+from routes import setup_routes
 
-app = routes.app
+DB_Auth = json.load(open("./secret/db_secret.json", "r", encoding="utf-8"))
+host = DB_Auth["connectionInfo"]["host"]
+user = DB_Auth["connectionInfo"]["user"]
+password = DB_Auth["connectionInfo"]["password"]
+db_name = DB_Auth["connectionInfo"]["db_name"]
+db_connection = pymysql.connect(host=host, user=user, password=password, database=db_name)
+
+app = Bottle()
+setup_routes(app, db_connection)
 
 if __name__ == '__main__':
-  app.run(host='localhost', port=8080)
+  try:
+    app.run(host='localhost', port=8080)
+  finally:
+    db_connection.close()
+
+
+  
--- routes.py
+++ routes.py
@@ -1,21 +1,21 @@
-from bottle import Bottle, template, static_file
+from bottle import template, static_file
 from function.web_scraping import get_Rakuten_Billing_Statement_Csv
 from function.save_billing_statement import save_billing_statement
-app = Bottle()
 
-@app.route('/static/<filePath:path>')
-def provide_StaticFile(filePath):
-  return static_file(filePath, root='./static/')
+def setup_routes(app, db_connection):
+  @app.route('/static/<filePath:path>')
+  def provide_StaticFile(filePath):
+    return static_file(filePath, root='./static/')
 
-@app.get("/")
-def get_And_Save_Latest_Billing_statement_And_Display_Latest_Payment():
-  # スクレイピングで取得
-  get_Rakuten_Billing_Statement_Csv()
+  @app.get("/")
+  def get_And_Save_Latest_Billing_statement_And_Display_Latest_Payment():
+    # スクレイピングで取得
+    #get_Rakuten_Billing_Statement_Csv()
 
-  # 明細をDBに保存
-  save_billing_statement()
+    # 明細をDBに保存
+    aaa = save_billing_statement(db_connection)
 
-  # 最新月分の支払いデータをドーナツグラフで表示
-  return template("payment_Latest_Month", hoge="AAAAAAAAA")
+    # 最新月分の支払いデータをドーナツグラフで表示
+    return template("payment_Latest_Month", hoge=aaa)
 
 
