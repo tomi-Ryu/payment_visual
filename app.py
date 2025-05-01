0e4776337a7c8c980085ff472c060e3d360eb54d
tomi-Ryu <ryusuke.tomita.forjob@gmail.com>
2025-05-01 10:51:44 +0900

    スクレイピングデータ取得・保存は、アプリ起動直後の1度のみ

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pymysql
 from bottle import Bottle
 from routes import setup_routes
 from const.const import DB_AUTH
+from function.web_scraping import get_Enavi_Billing_Statement_Csv
+from function.save_billing_statement import save_billing_statement
 
 # 辞書型変数DB_AUTHを**で展開
 db_connection = pymysql.connect(**DB_AUTH)
@@ -11,6 +13,16 @@ setup_routes(app, db_connection)
 
 if __name__ == '__main__':
   try:
+    with db_connection.cursor() as cursor:
+      cursor.execute("SELECT calculate_Billing_Statement_Download_Span()")
+      download_Span = cursor.fetchone()[0]
+
+    # スクレイピングで取得。取得明細数はdownload_Span
+    get_Enavi_Billing_Statement_Csv(download_Span)
+
+    # 明細をDBに保存
+    save_billing_statement(db_connection)
+
     app.run(host='localhost', port=8080)
   finally:
     db_connection.close()
--- routes.py
+++ routes.py
@@ -1,7 +1,5 @@
 import datetime
 from bottle import template, static_file
-from function.web_scraping import get_Enavi_Billing_Statement_Csv
-from function.save_billing_statement import save_billing_statement
 
 def setup_routes(app, db_connection):
   @app.route('/static/<filePath:path>')
@@ -10,16 +8,6 @@ def setup_routes(app, db_connection):
 
   @app.get("/")
   def get_And_Save_Latest_Billing_statement_And_Display_Latest_Payment():
-    with db_connection.cursor() as cursor:
-      cursor.execute("SELECT calculate_Billing_Statement_Download_Span()")
-      download_Span = cursor.fetchone()[0]
-
-    # スクレイピングで取得。取得明細数はdownload_Span
-    #get_Enavi_Billing_Statement_Csv(download_Span)
-
-    # 明細をDBに保存
-    #save_billing_statement(db_connection)
-    
     for_Cache_Buster = str(datetime.datetime.now())
     YYYY_hyphen_MM = for_Cache_Buster[:7]
 
