7c4c3f183042d4f11e7502215d504fbdab5998ad
tomi-Ryu <ryusuke.tomita.forjob@gmail.com>
2025-05-02 22:03:01 +0900

    使用頻度月1回未満の場合に対応

--- sql/func/calculate_Billing_Statement_Download_Span.sql
+++ sql/func/calculate_Billing_Statement_Download_Span.sql
@@ -1,26 +1,33 @@
 /*
-	if 明細データなし(基本的に、一度もダウンロードしていないのと同義)
-		return 12 (最新月含む最大1年間分。1年間分DLできないならできるだけDLする)
-	else:
-		return 1 (最新月分のみ)
+if 明細データなし(基本的に、一度もダウンロードしていないのと同義)
+	return 12 (最新月含む最大1年間分。1年間分DLできないならできるだけDLする)
+else:
+	return min(12, DB保存明細データの最新月から本コード実行時の月までの月数<ex:202503~202505→03/04/05の3つ>)
 */
 USE PaymentVisual;
 DELIMITER //
 CREATE FUNCTION calculate_Billing_Statement_Download_Span()
 RETURNS TINYINT
 BEGIN
-	DECLARE download_Span TINYINT;
-	DECLARE exist_Billing_Statement_Flg BOOLEAN;
+	DECLARE max_yyyymm_saved CHAR(6);
+	DECLARE diff_Month TINYINT;
 
-	# 明細データがあるかどうか判定
-	SELECT CASE COUNT(id) WHEN 0 THEN 0 ELSE 1 END INTO exist_Billing_Statement_Flg
+	# 明細データがあるかどうか判定。なければnullだがあればyyyymm
+	SELECT DATE_FORMAT(max(useDate), "%Y%m") INTO max_yyyymm_saved
 	FROM Billing_Statement_Rakuten;
 
-	IF exist_Billing_Statement_Flg = 0
+	IF max_yyyymm_saved IS NULL
 	THEN 
 		RETURN 12;
 	ELSE
-		RETURN 1;
+		SET diff_Month = PERIOD_DIFF(DATE_FORMAT(curdate(), "%Y%m"), max_yyyymm_saved) + 1;
+
+		IF diff_Month < 12
+		THEN
+			RETURN diff_Month;
+		ELSE
+			RETURN 12;
+		END IF;
 	END IF;
 
 END//
--- sql/proc/update_Billing_Statement.sql
+++ sql/proc/update_Billing_Statement.sql
@@ -1,6 +1,6 @@
 /*
-スクレイピングで得た明細情報を保存。
-最新月の情報は更新に近いことをする。つまりDeleteしてからInsert。
+	スクレイピングで得た明細情報を保存。
+	より詳細には、スクレイピングで得た情報の月単位の期間(ex: 202501~202502)のテーブルレコードを消してから、情報保存。
 */
 USE PaymentVisual;
 DELIMITER //
@@ -11,6 +11,7 @@ BEGIN
 	DECLARE restCnt_Statement INT;
 	DECLARE statement JSON;
 	DECLARE latest_Statement_Day DATE;
+	DECLARE earliest_Statement_Day DATE;
 	CREATE TEMPORARY TABLE Statement_Tbl (
 		dt date,
 		paymentTarget char(250),
@@ -27,20 +28,25 @@ BEGIN
 			SET restCnt_statement = restCnt_statement - 1;
 		END WHILE;
 
-		## 最新月の明細情報は全部消す
+		## スクレイピング情報の月単位期間と被るテーブルレコードは全部消す
 		# まず最新日の取得
-		SELECT dt INTO latest_Statement_Day
-		FROM Statement_Tbl
-		ORDER BY dt DESC
-		LIMIT 1;
-		# 最新日を利用し最新月の明細情報消去
+		SELECT max(dt) INTO latest_Statement_Day
+		FROM Statement_Tbl;
+		# 一番古い日の取得
+		SELECT min(dt) INTO earliest_Statement_Day
+		FROM Statement_Tbl;
+		# 明細情報テーブルレコード消去
 		DELETE FROM Billing_Statement_Rakuten
-		WHERE LAST_DAY(latest_Statement_Day - INTERVAL 1 MONTH) < useDate;
+		WHERE LAST_DAY(earliest_Statement_Day - INTERVAL 1 MONTH) < useDate AND useDate <= LAST_DAY(latest_Statement_Day);
 
 		# 明細情報保存。内容が同じ明細を複数DLした可能性を考慮し、DISTINCT
 		INSERT Billing_Statement_Rakuten(useDate, paymentTarget, cost)
 		SELECT DISTINCT dt, paymentTarget, cost
 		FROM Statement_Tbl;
 	END IF;
+	
+	# プロシージャ単体で複数回実行することもあるから、消しておこう。
+	DROP TABLE Statement_Tbl;
+	
 END//
 DELIMITER ;
